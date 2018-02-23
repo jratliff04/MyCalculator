f6bac32d947a8f3bffc7b3ac7008d0adcc250bf8
jratliff04 <jeremy_ratliff@hotmail.com>
2018-02-23 16:01:54 -0500

    Add files via upload

---
+++ mycalculator.js
@@ -0,0 +1,39 @@
+
+var userName = window.prompt("Please enter your name");
+
+var greetingParagraph = document.getElementById("greeting");
+
+greetingParagraph.innerHTML += userName;
+
+var num1 = parseInt(window.prompt("Enter a number:"));
+
+var num2 = parseInt(window.prompt("Enter another number:"));
+
+var span1 = document.getElementById("operand1");
+
+var span2 = document.getElementById("operand2");
+
+span1.innerHTML = num1;
+
+span2.innerHTML = num2;
+
+var sum = num1 + num2;
+
+var difference = num1 - num2;
+
+var product = num1 * num2;
+
+var quotient = num1 / num2;
+quotient = quotient.toFixed(2);
+
+var modResult = num1 % num2
+
+document.getElementById("addition").innerHTML = "The sum of " + num1 + " and " + num2 + " is: " + sum;
+
+document.getElementById("subtraction").innerHTML = "The difference of " + num1 + " and " + num2 + " is: " + difference;
+
+document.getElementById("multiplication").innerHTML = "The product of " + num1 + " and " + num2 + " is: " + product;
+
+document.getElementById("division").innerHTML = "The quotient of " + num1 + " and " + num2 + " is: " + quotient;
+
+document.getElementById("modulus").innerHTML = "The modulus of " + num1 + " and " + num2 + " is: " + modResult;
